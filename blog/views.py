2502703a31c3169e643de068910990951c92ff26
xiaoyue8 <xiaoyue8@outlook.com>
2018-12-16 16:58:30 +0800

    3.创建模板：1.第一种方式

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader,Context
 def index(req):
-	return HttpResponse('<h1>hello welcome to Django!</h1>')
+	t = loader.get_template('index.html')
+	c = Context({})
+	
+	# return HttpResponse('<h1>hello welcome to Django!</h1>')
+	return HttpResponse(t.render(c))
