bd434d3a0902eacbd3063df5c5a6954fc15b4dbb
xiaoyue8 <xiaoyue8@outlook.com>
2018-12-16 17:17:34 +0800

    3.创建模板：2.第二种方式render_to_response

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader,Context
+from django.shortcuts import render_to_response
+
+
+'''
 def index(req):
 	t = loader.get_template('index.html')
 	c = Context({})
 	
 	# return HttpResponse('<h1>hello welcome to Django!</h1>')
 	return HttpResponse(t.render(c))
+'''
+
+def index(req):
+	return render_to_response('index.html',{})
